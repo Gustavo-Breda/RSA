370fcece3b89eccce1b5829260eeab4cbfa7b444
Gustavo <gteixeirabreda@gmail.com>
2023-09-07 22:56:58 -0300

    first commit

---
+++ Bases.py
@@ -0,0 +1,362 @@
+from random import randint, randrange
+from math import log, isqrt
+
+
+class Theorem: 
+
+    def Korselt (boundary: int) -> list: 
+        ''' KORSELT method to CARMICHAEL numbers lower than the boundary '''
+
+        composite_numbers = Prime.Eratosthenes_C_sieve(boundary)
+        Carmichael_numbers = []
+        
+        ## TAKE THE COMPOSITE NUMBERS WHOSE SATISFIES THE KORSELT THEOREM
+        for composite in composite_numbers: 
+            
+            factors = Prime.factors (composite)
+
+            c = True
+            for factor in factors: 
+                
+                ## CONDITION 1: (factor * factor) NOT DIVIDE (c)
+                condition_two = (composite % (factor * factor) == 0)
+
+                ## CONDITION 2: THE GENERATOR CLASSES DIVIDES ORDER OF
+                condition_one = ((composite - 1) % (factor - 1) != 0)
+
+                ## IF BOTH TRUE WE HAVE CARMICHAEL NUMBER
+                if (condition_one or condition_two): 
+                    c = False
+                    break
+
+            if (c == True): 
+                Carmichael_numbers.append(composite)
+
+        return (Carmichael_numbers)
+
+    def Carmichael (boundary: int) -> list: 
+        ''' Naive method to CARMICHAEL numbers lower than the boundary '''
+
+        ## TAKE THE COMPOSITE PSEUDO-PRIMES
+        Carmichael_numbers = [pp for pp in Fermat.pseudo_primes (boundary) if len (Prime.factors (pseudo_prime)) > 1]
+
+        return (Carmichael_numbers)
+
+    def Euclides (a: int, b: int) -> (int, int, int): 
+        ''' Return the result of the Extended Euclidian Algorithm '''
+
+        if a == 0: 
+            return b, 0, 1
+
+        if b == 0: 
+            return a, 1, 0
+
+        # INIT THE ALGORITHM VALUES
+        dividend, divisor = a, b
+        x, x_one = 1, 0
+        y, y_one = 0, 1
+
+        while True: 
+            
+            # UPDATE THE CORE ALGORITHM VALUES
+            curr_quotient, curr_remainder = divmod (dividend, divisor)
+            x_two = (x - (x_one * curr_quotient))
+            y_two = (y - (y_one * curr_quotient))
+
+            # REACH TO GCD (a, b)
+            if curr_remainder == 0: 
+                return divisor, x_one, y_one
+            
+            # UPDATE THE ALGORITHM VALUES
+            dividend, divisor = divisor, curr_remainder
+            x, x_one = x_one, x_two
+            y, y_one = y_one, y_two
+
+class Fermat: 
+
+    def composite_test_one (number: int, random=None) -> str: 
+        ''' FERMAT test to assess if NUMBER is COMPOSITE '''
+        
+        if (random == None): 
+            random = randint (2, number)
+
+        if (pow (random, number, number) == (random % number)): 
+            return ("Inconclusive")
+        else: 
+            return ("True")
+            
+    def composite_test_two (number: int, random=None) -> str: 
+        ''' FERMAT test to assess if NUMBER is COMPOSITE '''
+        
+        if (random == None): 
+
+            random = randint (2, number - 1)
+
+            if ((random % number) == 0): 
+                random = randint (2, number - 1)
+
+        if ((random % number) == 0): 
+            raise ValueError ("The basis and number has to be co-primes")
+        
+        if (pow (random, number - 1, number) == 1): 
+            return ("Inconclusive")
+        else: 
+            return ("True")
+
+    def pseudo_primes (boundary: int) -> list: 
+        ''' Return the PSEUDO-PRIMES lower than the boundary '''
+        
+        pseudo_primes = [2]
+        
+        for odd_number in range (3, boundary, 2): 
+
+            for basis in range (2, odd_number + 1): 
+                
+                if (Fermat.composite_test_one (odd_number, basis) == "True"):
+                    break
+                
+                if (basis == odd_number): 
+                    pseudo_primes.append (odd_number)
+                    break
+        
+        return (pseudo_primes)
+
+    def factors (number: int) -> (int, int): 
+        ''' FERMAT method to return two factors of NUMBER '''
+
+        ## THE METHOD JUST TAKES ODD NUMBERS
+        if (number % 2 == 0): 
+            return (number // 2, number // 2)
+
+        ## INIT THE X VALUE TO COMPUTE
+        x = math.isqrt(number)
+        if (x == number): 
+            return (x, x)
+
+        while (True): 
+            
+            x += 1
+            y = math.isqrt((x * x) - number)
+
+            ## THE Y IS INTEGER AND WE FOUND TWO FACTORS
+            if ((y * y) == ((x * x) - number)): 
+
+                return (x + y, x - y)
+            
+            ## THE NUMBER IS PRIME
+            if (x == (number + 1) // 2): 
+
+                factors = {number: 1}
+                return (number, 1)
+           
+class Prime: 
+
+    def Eratosthenes_C_sieve (boundary: int) -> list: 
+        ''' Return COMPOSITE numbers less than the boundary '''
+
+        sieve_vector = [False] * (boundary + 1) 
+
+        ## RECORD THE EVEN COMPOSITE NUMBERS
+        for number in range (4, boundary + 1, 2): 
+
+            sieve_vector [number] = True
+
+        ## RECORD THE OOD COMPOSITE NUMBERS
+        for number in range (3, (int (boundary ** 0.5)) + 1, 2): 
+
+            if (sieve_vector [number] == False): 
+                
+                for c in range (number * number, boundary + 1, (2 * number)):
+                    sieve_vector [c] = True
+
+        composite_vector = [num for num, number in enumerate (sieve_vector) if number]
+
+        return (composite_vector)
+
+    def Eratosthenes_P_sieve (boundary: int) -> list: 
+        ''' Return PRIME numbers less than the boundary '''
+
+        sieve_vector = [True] * (boundary + 1) 
+
+        for number in range (4, boundary + 1, 2): 
+
+            sieve_vector [number] = False
+
+        for number in range (3, (int (boundary ** 0.5)) + 1, 2): 
+
+            for c in range (number * number, boundary + 1, (2 * number)): 
+
+                sieve_vector [c] = False
+
+        sieve_vector [0] = sieve_vector [1] = False
+
+        prime_vector = [number for number, prime in enumerate (sieve_vector) if prime]
+
+        return (prime_vector)
+
+
+    def Hadamard_number (x: int, e: int) -> int: 
+        ''' Return the amount of PRIME numbers between (x) and (x + e) '''
+
+        minimum_value = 10 ** 50
+        if (x < minimum_value): 
+            raise (ValueError (f"The number is too small, should be at least {minimum_value}"))
+
+        H = int (e // (math.log (x)))
+        return (H)
+
+    def Mersenne_number (n: int) -> int: 
+        ''' Return the MERSENNE number (M(N) = (2^N) - 1) '''
+
+        M = (2**n) - 1
+        return (M)
+    
+    def Fermat_number (n: int) -> int: 
+        ''' Return the FERMAT number (F(N) = (2^N) + 1) '''
+
+        F = (2**n) + 1
+        return (F)
+    
+
+    def test_MRA (number: int, tests: list = None) -> str: 
+        ''' MILLER-RABIN test whose assess if NUMBER is COMPOSITE '''
+
+        k = (number - 1)
+        even_factors = 0
+
+        if (tests == None): 
+
+            tests = []
+            for i in range (10): 
+
+                number = randint (2, number)
+                tests.append (number)
+
+        ## COUNT HOW MANY EVEN FACTORS THE (number - 1) HAS
+        while ((k % 2) == 0): 
+
+            k //= 2
+            even_factors += 1
+
+        ## THE TESTS CAN UNLUCKY TOOK 10 STRONG PSEUDOPRIMES TO EACH BASIS
+        for basis in tests: 
+
+            remainder = pow (basis, k, number)
+
+            ## CONTRAPOSITION OF FERMAT THEOREM
+            if ((remainder != 1) and (remainder != number - 1)):  
+
+                for factor in range (even_factors + 1): 
+
+                    remainder = ((remainder ** 2) % number)
+                    
+                    if ((remainder == number - 1) or (remainder == 1)): 
+                        break
+
+                    ## WE REACHED TO THE LAST EVEN FACTOR
+                    if (factor == even_factors): 
+                        return 'False'
+
+        return ('inconclusive')
+
+    def test_BLS (number: int) -> bool: 
+        ''' Test of 3 mathmaticians whose assess if NUMBER is PRIME '''
+
+        if (number == 2): 
+            return True
+
+        n = number - 1
+        n_factors = Prime.factors (n)
+
+        for n_factor in n_factors: 
+
+            for b in range (3, n + 1): 
+                
+                if (pow (b, n, number) != 1): 
+                    return False
+
+                if (pow (b, (n // n_factor), number) != 1): 
+                    break
+                
+                if (b == n): 
+                    return False
+
+        return True
+
+
+    def factors (number: int) -> dict: 
+        ''' Return the PRIME factors as dict  '''
+        
+        factors = {}
+        iterator = Prime.iterator ()
+        iterator_number = next (iterator)
+
+        if (number < 2):
+            raise ValueError ("Prime numbers are >= 2")
+        if (number == 2): 
+            return ({2: 1})
+        if (number == 3): 
+            return ({3: 1})
+        
+        try: 
+            
+            while (number > 1): 
+                
+                ## ASSES IF THE NUMBER IS COMPOSITE
+                if (number % iterator_number == 0): 
+                    number //= iterator_number
+                    
+                    if (iterator_number not in factors): 
+                        factors [iterator_number] = 1
+                    else: 
+                        factors [iterator_number] += 1
+                else: 
+                    iterator_number = next (iterator)
+                
+                ## WE REACH TO THE LAST POSSIBLE PRIME DIVISOR
+                if (iterator_number ** 2 > number): 
+                    
+                    if (iterator_number % number == 0): 
+                        factors [iterator_number] += 1   
+                    else: 
+                        factors [number] = 1
+                        
+                    return (factors)
+                
+        except KeyboardInterrupt: 
+            raise ValueError ("Value not factorized")
+
+    def iterator () -> int: 
+        ''' Function whose return PRIMES each time called '''
+
+        iterator_list = []
+        current = 2 
+        
+        while True: 
+
+            ## IF (N) IS COMPOSITE SO THE LOWER FACTOR (F) IS (F <= N**0.5)
+            break_point = current ** 0.5
+            is_prime = True
+
+            ## TAKE THE MANUEL DIVISION TO ASSES IF IS COMPOSITE
+            for prime in iterator_list: 
+                
+                if (prime > break_point): 
+                    break
+                
+                if (current % prime == 0): 
+                    is_prime = False
+                    break
+            
+            ## FOUND THE PRIME AND YIELD
+            if (is_prime): 
+                iterator_list.append (current)
+                yield current
+                
+            current += 1
+
+
+if (__name__ == '__main__'): 
+
+    n = int (input ("Erastothenes sieve to: "))
+    print (f"Primes: {Prime.Eratosthenes_C_sieve (n)}")
---
+++ RSA.py
@@ -0,0 +1,166 @@
+from Bases import *
+from DCS import *
+
+
+# ------------------------------------------------------------------
+# Utility Functions Section
+# ------------------------------------------------------------------
+
+def chinese_remainder_theorem (number: int, p: int, q: int, d: int) -> int: 
+    ''' Resolution through Chinese Remainder Theorem '''
+
+    ## REDUCED EXPOENTS
+    dp = d % (p - 1)
+    dq = d % (q - 1)
+
+    ## TAKE THE INVERSES
+    _, inv_p, inv_q = EEA_natural (p, q)
+
+    ## REDUCED THE MESSAGES
+    reduced_number_p = number % p
+    reduced_number_q = number % q
+
+    count_a = pow (reduced_number_p, dp, p)
+    count_b = pow (reduced_number_q, dq, q)
+    number = (count_a + (count_b - count_a) * inv_p)
+
+    return (number)
+
+def EEA_natural (a: int, b: int) -> (int, int, int): 
+    ''' Return the natural results of Extended Euclidian Algorithm '''
+
+    abs_a = abs (a)
+    abs_b = abs (b)
+
+    gcd, x, y = Theorem.Euclides (abs_a, abs_b)
+    x %= abs_b
+    y %= abs_a
+    
+    return (gcd, x, y)
+
+def RSA_test (number: int) -> str: 
+    ''' RSA tests to assess if number is PRIME ''' 
+
+    iterator = Prime.iterator ()
+    current = next (iterator)
+    iterator_list = []
+
+    ## TEST IF HAS FACTOR LOWER THAN 5000
+    while (current < 5000): 
+        
+        iterator_list.append(current)
+        if (number % current == 0): 
+            return False
+    
+        current = next (iterator)
+    
+    ## TEST 10 OF THESE PRIMES AS BASIS IN MILLER-RABIN
+    test_list = []
+    for i in range (10): 
+        test_list.append (iterator_list [randint (1, 5000) % len (iterator_list)])
+    if (Prime.test_MRA (number, test_list) == 'False'): 
+        return ('False')
+    
+    return ('inconclusive')
+
+def random_prime (r: int) -> int: 
+    ''' Return a PROBABLY (r) digits PRIME  '''
+
+    while True: 
+
+        random = int (randrange ((10 ** r) + 1, (10 ** r) * 100, 2))
+
+        if (RSA_test (random) == 'inconclusive'):
+            return (random)
+
+
+# -------------------------------------------------------------------
+# RSA Functions Section
+# -------------------------------------------------------------------
+
+def generate_keys (): 
+
+    ## THE PRIME NUMBERS WHOSE FORM MODULO N
+    p = random_prime (125)
+    q = random_prime (105)
+
+    ## THE TOTAL OF INVERSES MODULO N
+    E = (p - 1) * (q - 1)
+    
+    e = 3
+    while (True): 
+        
+        ## FIND SMALLER KEY WHOSE HAS INVERSE MODULO E
+        gcd, d, _ = EEA_natural (e, E)
+        
+        ## THE PUB_KEY WAS FOUND
+        if (gcd == 1): 
+            break
+        
+        e += 2
+
+    return (p * q, p, q, e, d)
+
+def encrypt (text: str, e: int, modulo: int) -> list: 
+
+    ## CHOOSE THE SIZE OF EACH SECTION LESS THAN THE MIN(p, q) 
+    section_size = 101 // 3
+    
+    ## DECODE THE SYMBS IN TEXT
+    text_symb_list = [symb_to_code (symb) for symb in list (text)]
+
+    ## SEPARATE THE SYMBS IN SECTIONS
+    text_code_list = [''.join(text_symb_list [i:i+section_size]) for i in range (0, len (text_symb_list), section_size)]
+
+    ## EBCRYPT THE SECTIONS
+    c_text = [int (code) for code in text_code_list]
+    c_text = [pow (section, e, modulo) for section in c_text]
+
+    return (c_text)
+
+def decrypt_naive (e_text: list, d: int, modulo: int) -> str: 
+
+    ## DECRYPT THE SECTIONS
+    dt_code_sections = [str (pow (section, d, modulo)) for section in e_text]
+    
+    ## SEPARATE THE SECTIONS IN 3 NUMBERS AGAIN
+    dt_codes_value = [section [i:i+3] for section in dt_code_sections for i in range (0, len (section), 3)]
+    dt_codes_value = [int (code) for code in dt_codes_value]
+    
+    ## ENCODE THE TEXT
+    dt_text = [code_to_symb (symb) for symb in dt_codes_value]            
+
+    text = ''    
+    for symb in dt_text: 
+        text += symb
+            
+    return (text)
+
+def decrypt_chinese (e_text: list, p: int, q: int, d: int) -> str: 
+
+    ## DECRYPT THE SECTIONS WITG THE CHINESE REMAINDER THEOREM
+    dt_code_sections = [str (chinese_remainder_theorem (section, p, q, d)) for section in e_text]
+
+    ## SEPARATE THE SECTIONS IN 3 NUMBERS AGAIN
+    dt_codes_value = [section [i:i+3] for section in dt_code_sections for i in range (0, len (section), 3)]
+    dt_codes_value = [int (code) for code in dt_codes_value]
+    
+    ## DECODE THE TEXT
+    dt_text = [code_to_symb (symb) for symb in dt_codes_value]            
+
+    text = ''    
+    for symb in dt_text: 
+        text += symb
+            
+    return (text)
+
+
+if (__name__ == '__main__'): 
+    
+    modulo, p, q, e, d = generate_keys ()
+
+    text = input ("Text to encrypt: ")
+    encrypted_text = encrypt (text, e, modulo)
+
+    print (f"\nEncrypted text: {encrypted_text}")
+    print (f"Decrypted text: {chinese_decrypt (encrypted_text, p, q, d)}\n")
